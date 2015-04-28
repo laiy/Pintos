8cf7724449eb8c6d248a6522ab1899686815a543
laiy <ly.franky@gmail.com>
2015-04-28 13:52:57 +0800

    fixed

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -26,6 +26,7 @@ typedef int fixed_t;
 /* Get integer part of a fixed-point value. */
 #define FP_INT_PART(A) (A >> FP_SHIFT_AMOUNT)
 /* Get rounded integer of a fixed-point value. */
-#define FP_ROUND(A) (A >= 0 ? ((A + (1 << (FP_SHIFT_AMOUNT - 1))) >> FP_SHIFT_AMOUNT) \ : ((A - (1 << (FP_SHIFT_AMOUNT - 1))) >> FP_SHIFT_AMOUNT))
+#define FP_ROUND(A) (A >= 0 ? ((A + (1 << (FP_SHIFT_AMOUNT - 1))) >> FP_SHIFT_AMOUNT) \
+        : ((A - (1 << (FP_SHIFT_AMOUNT - 1))) >> FP_SHIFT_AMOUNT))
 
 #endif /* thread/fixed_point.h */
